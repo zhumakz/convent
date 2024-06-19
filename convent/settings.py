3956dac6430993fac4ac98f94d8463fd8c677493
Rnurdaulet <rakhymberdin@gmail.com>
2024-06-19 19:43:55 +0500

    Implement features for main section of site

--- convent/settings.py
+++ convent/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'accounts',
     'friends',
     'coins',
+    'main',
 ]
 
 MIDDLEWARE = [
--- convent/urls.py
+++ convent/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
     path('accounts/', include('accounts.urls')),
     path('friends/', include('friends.urls')),
     path('coins/', include('coins.urls')),
