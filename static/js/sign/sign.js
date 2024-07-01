63d80bce100bf0c8e2ee4314269b89e7219ccd78
Rnurdaulet <rakhymberdin@gmail.com>
2024-07-01 23:31:20 +0500

    login.html

--- static/js/sign/sign.js
+++ static/js/sign/sign.js
@@ -1,5 +1,7 @@
 const errorMessage = document.querySelector(".error-message");
 
+const errorMessagePopup = document.querySelector(".error-message-popup");
+
 function showError(message) {
     errorMessage.textContent = message;
     errorMessage.style.display = 'block';
@@ -7,4 +9,13 @@ function showError(message) {
 
 function hideError() {
     errorMessage.style.display = 'none';
+}
+
+function showErrorPopup(message) {
+    errorMessagePopup.textContent = message;
+    errorMessagePopup.style.display = 'flex';
+}
+
+function hideErrorPopup() {
+    errorMessagePopup.style.display = 'none';
 }
