65b49bc53a8bdf7941250cf59b10a5e40c49bd33
Rnurdaulet <rakhymberdin@gmail.com>
2024-07-03 19:53:30 +0500

    qr handler Friends

--- qr_handler/urls.py
+++ qr_handler/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from django.views.generic import TemplateView
-from .views import qr_scan_view, handle_qr_data, operation_error
+from .views import qr_scan_view, handle_qr_data, qr_friend_request, qr_purchase_detail, qr_campaign_vote, \
+    qr_lecture_detail
 
 urlpatterns = [
     path('qr-scan/', qr_scan_view, name='qr_scan'),
     path('handle-qr-data/', handle_qr_data, name='handle_qr_data'),
-    path('friend-request-success/', TemplateView.as_view(template_name='qr_handler/friend_request_success.html'), name='friend_request_success'),
-    path('lecture-start-success/', TemplateView.as_view(template_name='lecture_start_success.html'), name='lecture_start_success'),
-    path('lecture-end-success/', TemplateView.as_view(template_name='lecture_end_success.html'), name='lecture_end_success'),
-    path('purchase-success/', TemplateView.as_view(template_name='purchase_success.html'), name='purchase_success'),
-    path('event-success/', TemplateView.as_view(template_name='event_success.html'), name='event_success'),
-    path('operation-error/', operation_error, name='operation_error'),
+    path('qr_friend_request/', qr_friend_request, name='qr_friend_request'),
+    path('qr_purchase_detail/<int:purchase_id>/', qr_purchase_detail, name='qr_purchase_detail'),
+    path('qr_campaign_vote/<str:campaign_vote>/', qr_campaign_vote, name='qr_campaign_vote'),
+    path('qr_lecture_detail/<str:lecture_start>/<str:lecture_end>/', qr_lecture_detail, name='qr_lecture_detail'),
 ]
--- qr_handler/views.py
+++ qr_handler/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
@@ -14,178 +15,227 @@ from shop.models import Purchase
 
 logger = logging.getLogger(__name__)
 
-
+@login_required
+def test_page(request):
+    return render(request, 'qr_handler/test_page.html')
 @login_required
 def qr_scan_view(request):
+    # Отображение страницы с формой для сканирования QR-кода
     return render(request, 'qr_handler/qr.html')
-
+#
+# @login_required
+# def handle_qr_data(request):
+#     if request.method == 'POST':
+#         qr_data = request.POST.get('qr_data')
+#         if qr_data:
+#             try:
+#                 data = json.loads(qr_data)
+#                 user_id = data.get('user_id')
+#                 lecture_start = data.get('lecture_start')
+#                 lecture_end = data.get('lecture_end')
+#                 purchase_id = data.get('purchase_id')
+#                 campaign_vote = data.get('campaign_vote')
+#
+#                 # Логика перенаправления в зависимости от данных QR-кода
+#                 if user_id:
+#                     return redirect('qr_friend_request')
+#                 elif purchase_id:
+#                     return redirect('qr_purchase_detail', purchase_id=purchase_id)
+#                 elif campaign_vote:
+#                     return redirect('qr_campaign_vote', campaign_vote=campaign_vote)
+#                 elif lecture_start and lecture_end:
+#                     return redirect('qr_lecture_detail', lecture_start=lecture_start, lecture_end=lecture_end)
+#                 else:
+#                     return redirect('profile')
+#
+#             except json.JSONDecodeError:
+#                 # Обработка ошибки декодирования JSON
+#                 return JsonResponse({'error': 'Invalid QR data'}, status=400)
+#
+#     return redirect('profile')
+#
+# def handle_friend_request(request, user_id):
+#     try:
+#         to_user = get_object_or_404(User, id=user_id)
+#         from_user = request.user
+#
+#         if from_user == to_user:
+#             logger.warning("User tried to add themselves as a friend")
+#             message = 'You cannot add yourself as a friend.'
+#             return redirect('operation_error', message=message)
+#
+#         if to_user.is_admin or to_user.is_moderator:
+#             logger.warning("User tried to add an admin or moderator as a friend")
+#             message = 'You cannot add administrators or moderators as friends.'
+#             return redirect('operation_error', message=message)
+#
+#         if FriendService.are_friends(from_user, to_user):
+#             logger.warning("Users are already friends")
+#             message = 'You are already friends.'
+#             return redirect('operation_error', message=message)
+#
+#         friend_request = FriendRequest.objects.filter(from_user=to_user, to_user=from_user).first()
+#
+#         if friend_request:
+#             message = FriendService.confirm_friend_request(friend_request)
+#
+#         else:
+#             if FriendRequest.objects.filter(from_user=from_user, to_user=to_user).exists():
+#                 logger.warning("Friend request already sent")
+#                 message = 'Friend request already sent.'
+#                 return redirect('operation_error', message=message)
+#
+#             success, message = FriendService.send_friend_request(from_user, to_user)
+#
+#         return render(request, 'qr_handler/friend_request_success.html')
+#
+#     except Exception as e:
+#         logger.error("Error handling friend request: %s", e, exc_info=True)
+#         return redirect('operation_error', message=str(e))
+#
+#
+# def handle_lecture_start(request, lecture_id):
+#     try:
+#         user = request.user
+#         lecture = get_object_or_404(Lecture, id=lecture_id)
+#
+#         success, message = LectureService.register_lecture_start(user, lecture)
+#         if success:
+#             return JsonResponse({'status': 'ok', 'message': message})
+#         else:
+#             message = 'Friend request already sent.'
+#             return redirect('operation_error', message=message)
+#     except Exception as e:
+#         logger.error("Error handling lecture start: %s", e, exc_info=True)
+#         return redirect('operation_error', message=str(e))
+#
+#
+# def handle_lecture_end(request, lecture_id):
+#     try:
+#         user = request.user
+#         lecture = get_object_or_404(Lecture, id=lecture_id)
+#
+#         success, message = LectureService.register_lecture_end(user, lecture)
+#         if success:
+#             return JsonResponse({'status': 'ok', 'message': message})
+#         else:
+#             return JsonResponse({'status': 'error', 'message': message})
+#     except Exception as e:
+#         logger.error("Error handling lecture end: %s", e, exc_info=True)
+#         return redirect('operation_error', message=str(e))
+#
+#
+# def handle_purchase(request, purchase_id):
+#     try:
+#         user = request.user
+#         purchase = get_object_or_404(Purchase, id=purchase_id)
+#
+#         if user.doscointbalance.balance < purchase.amount:
+#             logger.warning("Insufficient balance to complete the purchase")
+#             return JsonResponse({'status': 'error', 'message': 'Insufficient balance to complete the purchase'})
+#
+#         Transaction.objects.create(
+#             sender=user,
+#             recipient=purchase.seller,
+#             amount=purchase.amount,
+#             description=f'Purchase of {purchase.product.name}',
+#             is_system_transaction=True
+#         )
+#         purchase.is_completed = True
+#         purchase.buyer = user
+#         purchase.save()
+#
+#         return JsonResponse({'status': 'ok', 'message': 'Purchase handled successfully'})
+#     except Exception as e:
+#         logger.error("Error handling purchase: %s", e, exc_info=True)
+#         return redirect('operation_error', message=str(e))
+#
+#
+# def handle_doscam_request(request, user_id):
+#     try:
+#         return JsonResponse({'status': 'error', 'message': 'handle_doscam_request'})
+#     except Exception as e:
+#         logger.error("Error handling event request: %s", e, exc_info=True)
+#         return redirect('operation_error', message=str(e))
+#
+#
 
 @login_required
 def handle_qr_data(request):
     if request.method == 'POST':
         qr_data = request.POST.get('qr_data')
+        if qr_data:
+            try:
+                data = json.loads(qr_data)
+                user_id = data.get('user_id')
+                lecture_start = data.get('lecture_start')
+                lecture_end = data.get('lecture_end')
+                purchase_id = data.get('purchase_id')
+                campaign_vote = data.get('campaign_vote')
 
-        if not qr_data:
-            logger.error("No QR data received")
-            return JsonResponse({'status': 'error', 'message': 'No QR data received'}, status=400)
+                # Сохранение данных в сессии
+                request.session['qr_data'] = data
 
-        try:
-            logger.info(f"Received QR data: {qr_data}")
-            data = json.loads(qr_data)
+                # Логика перенаправления в зависимости от данных QR-кода
+                if user_id:
+                    return redirect('qr_friend_request')
+                elif purchase_id:
+                    return redirect('qr_purchase_detail')
+                elif campaign_vote:
+                    return redirect('qr_campaign_vote')
+                elif lecture_start and lecture_end:
+                    return redirect('qr_lecture_detail')
+                else:
+                    return redirect('profile')
 
-            handlers = {
-                'user_id': handle_friend_request,
-                'lecture_start': handle_lecture_start,
-                'lecture_end': handle_lecture_end,
-                'purchase_id': handle_purchase,
-                'event_id': handle_doscam_request
-            }
+            except json.JSONDecodeError:
+                # Обработка ошибки декодирования JSON
+                return JsonResponse({'error': 'Invalid QR data'}, status=400)
 
-            for key, handler in handlers.items():
-                if key in data:
-                    return handler(request, data[key])
-
-            return JsonResponse({'status': 'error', 'message': 'Unknown QR data'})
-
-        except json.JSONDecodeError:
-            logger.error("Invalid QR data: %s", qr_data)
-            return JsonResponse({'status': 'error', 'message': 'Invalid QR data'})
-        except Exception as e:
-            logger.error("Error handling QR data: %s", e, exc_info=True)
-            return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
-
-    return JsonResponse({'status': 'error', 'message': 'Invalid request'}, status=400)
-
-
-def handle_friend_request(request, user_id):
-    try:
-        to_user = get_object_or_404(User, id=user_id)
-        from_user = request.user
-
-        if from_user == to_user:
-            logger.warning("User tried to add themselves as a friend")
-            message = 'You cannot add yourself as a friend.'
-            return redirect('operation_error', message=message)
-
-        if to_user.is_admin or to_user.is_moderator:
-            logger.warning("User tried to add an admin or moderator as a friend")
-            message = 'You cannot add administrators or moderators as friends.'
-            return redirect('operation_error', message=message)
-
-        if FriendService.are_friends(from_user, to_user):
-            logger.warning("Users are already friends")
-            message = 'You are already friends.'
-            return redirect('operation_error', message=message)
-
-        friend_request = FriendRequest.objects.filter(from_user=to_user, to_user=from_user).first()
-
-        if friend_request:
-            message = FriendService.confirm_friend_request(friend_request)
-
-        else:
-            if FriendRequest.objects.filter(from_user=from_user, to_user=to_user).exists():
-                logger.warning("Friend request already sent")
-                message = 'Friend request already sent.'
-                return redirect('operation_error', message=message)
-
-            success, message = FriendService.send_friend_request(from_user, to_user)
-
-        request.session['friend_request_data'] = {
-            'status': 'ok',
-            'message': 'Friend request sent successfully!',
-            'user_info': {
-                'name': f"{to_user.name} {to_user.surname}",
-                'location': to_user.city.name if to_user.city else '',
-                'profile_picture': to_user.profile_picture.url if to_user.profile_picture else None,
-                'qr_code': to_user.qr_code.url if to_user.qr_code else None
-            }
-        }
-        return redirect('friend_request_success')
-
-
-    except Exception as e:
-        logger.error("Error handling friend request: %s", e, exc_info=True)
-        return redirect('operation_error', message=str(e))
-
-
-def handle_lecture_start(request, lecture_id):
-    try:
-        user = request.user
-        lecture = get_object_or_404(Lecture, id=lecture_id)
-
-        success, message = LectureService.register_lecture_start(user, lecture)
-        if success:
-            return JsonResponse({'status': 'ok', 'message': message})
-        else:
-            message = 'Friend request already sent.'
-            return redirect('operation_error', message=message)
-    except Exception as e:
-        logger.error("Error handling lecture start: %s", e, exc_info=True)
-        return redirect('operation_error', message=str(e))
-
-
-def handle_lecture_end(request, lecture_id):
-    try:
-        user = request.user
-        lecture = get_object_or_404(Lecture, id=lecture_id)
-
-        success, message = LectureService.register_lecture_end(user, lecture)
-        if success:
-            return JsonResponse({'status': 'ok', 'message': message})
-        else:
-            return JsonResponse({'status': 'error', 'message': message})
-    except Exception as e:
-        logger.error("Error handling lecture end: %s", e, exc_info=True)
-        return redirect('operation_error', message=str(e))
-
-
-def handle_purchase(request, purchase_id):
-    try:
-        user = request.user
-        purchase = get_object_or_404(Purchase, id=purchase_id)
-
-        if user.doscointbalance.balance < purchase.amount:
-            logger.warning("Insufficient balance to complete the purchase")
-            return JsonResponse({'status': 'error', 'message': 'Insufficient balance to complete the purchase'})
-
-        Transaction.objects.create(
-            sender=user,
-            recipient=purchase.seller,
-            amount=purchase.amount,
-            description=f'Purchase of {purchase.product.name}',
-            is_system_transaction=True
-        )
-        purchase.is_completed = True
-        purchase.buyer = user
-        purchase.save()
-
-        return JsonResponse({'status': 'ok', 'message': 'Purchase handled successfully'})
-    except Exception as e:
-        logger.error("Error handling purchase: %s", e, exc_info=True)
-        return redirect('operation_error', message=str(e))
-
-
-def handle_doscam_request(request, user_id):
-    try:
-        return JsonResponse({'status': 'error', 'message': 'handle_doscam_request'})
-    except Exception as e:
-        logger.error("Error handling event request: %s", e, exc_info=True)
-        return redirect('operation_error', message=str(e))
-
+    return redirect('qr_handler:profile')
 
 @login_required
-def test_page(request):
-    return render(request, 'qr_handler/test_page.html')
+def qr_friend_request(request):
+    data = request.session.get('qr_data')
+    if not data:
+        return redirect('profile')
 
+    user_id = data.get('user_id')
 
-def operation_error(request, message):
-    return render(request, 'qr_handler/operation_error.html', {'message': message})
+    try:
+        to_user = User.objects.get(id=user_id)
+        success, message = FriendService.send_friend_request(request.user, to_user)
+        context = {
+            'message': message,
+            'success': success,
+            'to_user': to_user,
+            'qr_code_url': request.user.qr_code.url
+        }
+        return render(request, 'qr_handler/qr_friend_request.html', context)
+    except User.DoesNotExist:
+        return JsonResponse({'error': 'User not found'}, status=404)
+    except ValidationError as e:
+        context = {
+            'message': e.messages[0],
+            'success': False,
+            'to_user': None,
+            'qr_code_url': None,
+        }
+        return render(request, 'qr_handler/qr_friend_request.html', context)
+@login_required
+def qr_purchase_detail(request, purchase_id):
+    # Логика для отображения информации о покупке по QR-коду
+    context = {'purchase_id': purchase_id}
+    return render(request, 'qr_handler/qr_purchase_detail.html', context)
 
+@login_required
+def qr_campaign_vote(request, campaign_vote):
+    # Логика для обработки голосования в кампании по QR-коду
+    context = {'campaign_vote': campaign_vote}
+    return render(request, 'qr_handler/qr_campaign_vote.html', context)
 
-def friend_request_success(request):
-    friend_request_data = request.session.pop('friend_request_data', None)
-    if friend_request_data:
-        return render(request, 'qr_handler/friend_request_success.html', friend_request_data)
-    else:
-        # Если данных нет в сессии, обработка ошибки
-        return redirect('operation_error', message='Data not found')
+@login_required
+def qr_lecture_detail(request, lecture_start, lecture_end):
+    # Логика для отображения информации о лекции по QR-коду
+    context = {'lecture_start': lecture_start, 'lecture_end': lecture_end}
+    return render(request, 'qr_handler/qr_lecture_detail.html', context)
