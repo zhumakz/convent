465a651193f86ef2d0b8bd7c3872a1f6a06b78fd
Rnurdaulet <rakhymberdin@gmail.com>
2024-07-04 14:58:33 +0500

    Update static pGWS

--- static_pages/apps.py
+++ static_pages/apps.py
@@ -5,6 +5,6 @@ class StaticPagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'static_pages'
 
-    # def ready(self):
-    #     from .models import StaticPage
-    #     StaticPage.create_static_pages()
+    def ready(self):
+        from .models import StaticPage
+        StaticPage.create_static_pages()
