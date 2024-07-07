40b9890d02d1c1786b56b9d177f1d616780588a7
Rnurdaulet <rakhymberdin@gmail.com>
2024-07-07 19:21:31 +0500

    Lecture

--- lectures/urls.py
+++ lectures/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import lecture_list, lecture_detail, scan_lecture_qr_start, scan_lecture_qr_end, show_qr_start, show_qr_end
+from .views import lecture_list, lecture_detail, show_qr_start, show_qr_end
 
 urlpatterns = [
     path('', lecture_list, name='lecture_list'),
     path('<int:lecture_id>/', lecture_detail, name='lecture_detail'),
-    path('<int:lecture_id>/scan_start/', scan_lecture_qr_start, name='scan_lecture_qr_start'),
-    path('<int:lecture_id>/scan_end/', scan_lecture_qr_end, name='scan_lecture_qr_end'),
     path('<int:lecture_id>/show_qr_start/', show_qr_start, name='show_qr_start'),
     path('<int:lecture_id>/show_qr_end/', show_qr_end, name='show_qr_end'),
 ]
--- lectures/views.py
+++ lectures/views.py
@@ -16,32 +16,6 @@ def lecture_detail(request, lecture_id):
     return render(request, 'lectures/lecture_detail.html', {'lecture': lecture, 'attendance': attendance})
 
 @login_required
-def scan_lecture_qr_start(request, lecture_id):
-    lecture = LectureService.get_lecture_by_id(lecture_id)
-    user = request.user
-
-    success, message = LectureService.register_lecture_start(user, lecture)
-    if success:
-        messages.success(request, message)
-    else:
-        messages.error(request, message)
-
-    return redirect('lecture_detail', lecture_id=lecture_id)
-
-@login_required
-def scan_lecture_qr_end(request, lecture_id):
-    lecture = LectureService.get_lecture_by_id(lecture_id)
-    user = request.user
-
-    success, message = LectureService.register_lecture_end(user, lecture)
-    if success:
-        messages.success(request, message)
-    else:
-        messages.error(request, message)
-
-    return redirect('lecture_detail', lecture_id=lecture_id)
-
-@login_required
 def show_qr_start(request, lecture_id):
     lecture = LectureService.get_lecture_by_id(lecture_id)
     return render(request, 'lectures/show_qr_start.html', {'lecture': lecture})
