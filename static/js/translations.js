dd0bbcc48b7ba79f63975bdff0b2f3358fdf65bb
Zhassulan <zhas12345@gmail.com>
2024-07-09 10:21:14 +0500

    Create translations.js

---
+++ static/js/translations.js
@@ -0,0 +1,13 @@
+// translations.js
+export const translations = {
+    en: {
+        scanSuccess: "Scan was successful",
+        preloaderMessage: "Please wait...",
+        backButton: "Back"
+    },
+    ru: {
+        scanSuccess: "Сканирование успешно завершено",
+        preloaderMessage: "Пожалуйста, подождите...",
+        backButton: "Назад"
+    }
+};
